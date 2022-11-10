6d72608a36dbc2bba2e7605f3df3e5d9f8e2622d
connyycai <conny.cai@utexas.edu>
2022-11-09 19:44:44 -0600

    save

--- Week_8/testing_express/index.js
+++ Week_8/testing_express/index.js
@@ -7,4 +7,8 @@ app.get("/config", function (req, res) {
   res.status(200).json({ version: "0.0.1" });
 });
 
+app.post("/config", (req, res) => {
+  res.status(200).json({ version: "0.0.1" });
+});
+
 module.exports = app;
